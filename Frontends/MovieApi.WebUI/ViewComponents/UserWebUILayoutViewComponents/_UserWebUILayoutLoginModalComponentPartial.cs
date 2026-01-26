238653be7a343cad7356818e37db8061b0dcee6a
elmxan <elmxan.hemzeyev@gmail.com>
2026-01-26 09:14:06 +0400

    Create partial views

---
+++ Frontends/MovieApi.WebUI/ViewComponents/UserWebUILayoutViewComponents/_UserWebUILayoutLoginModalComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.UserWebUILayoutViewComponents
+{
+    public class _UserWebUILayoutLoginModalComponentPartial:ViewComponent  
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MovieApi.WebUI/ViewComponents/UserWebUILayoutViewComponents/_UserWebUILayoutRegisterModalComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.UserWebUILayoutViewComponents
+{
+    public class _UserWebUILayoutRegisterModalComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+    
+}
